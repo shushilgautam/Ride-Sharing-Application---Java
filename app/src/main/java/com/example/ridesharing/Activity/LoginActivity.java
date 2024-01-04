8b331063120609986dbb22f247d2b8329694b829
Bipana <bipanathapa355@gmail.com>
2024-01-04 15:54:25 +0545

    login and signup

--- app/src/main/java/com/example/ridesharing/Activity/LoginActivity.java
+++ app/src/main/java/com/example/ridesharing/Activity/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.ridesharing.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +13,31 @@ import com.example.ridesharing.R;
 
 
 public class LoginActivity extends AppCompatActivity {
+    TextView signup;
+    Button login;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginpage);
+
+        signup=findViewById(R.id.signupTextView);
+        login=findViewById(R.id.continebutton);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(i);
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this,HomeActivity.class);
+                startActivity(i);
+            }
+        });
+
 
     }
 }
--- app/src/main/java/com/example/ridesharing/Activity/SignupActivity.java
+++ app/src/main/java/com/example/ridesharing/Activity/SignupActivity.java
@@ -2,15 +2,30 @@ package com.example.ridesharing.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.ridesharing.R;
 
 public class SignupActivity extends AppCompatActivity {
+    Button log;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_layout);
+
+        log=findViewById(R.id.btnsignup);
+
+        log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SignupActivity.this,LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
