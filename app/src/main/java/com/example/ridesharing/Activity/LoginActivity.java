378afc08df6f2c80c312c5b10410a6d1ab036c89
Bipana <bipanathapa355@gmail.com>
2024-01-04 14:34:17 +0545

    login as launcher

--- app/src/main/java/com/example/ridesharing/Activity/LoginActivity.java
+++ app/src/main/java/com/example/ridesharing/Activity/LoginActivity.java
@@ -5,10 +5,14 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.ridesharing.R;
+
+
 public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView();
+        setContentView(R.layout.loginpage);
+
     }
 }
